e5c2eda39414457776e1bddc0a4311a23cf49447
Kana0222017 <kana0222017@gmail.com>
2025-07-11 13:46:44 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let visits = localStorage.getItem("visitCount") || 0;
-    visits++;
-    localStorage.setItem("visitCount", visits);
-    console.log(`This user has visited ${visits} time(s).`);
-});
