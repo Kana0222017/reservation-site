9cac854c7c33924a1f2a5af88f05b78d642dbf57
Kana0222017 <kana0222017@gmail.com>
2025-07-11 13:24:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     let visits = localStorage.getItem("visitCount") || 0;
     visits++;
     localStorage.setItem("visitCount", visits);
-    const counter = document.getElementById("visit-counter");
-    if (counter) {
-        counter.textContent = `You have visited this site ${visits} time(s).`;
-    }
+    console.log(`This user has visited ${visits} time(s).`);
 });
